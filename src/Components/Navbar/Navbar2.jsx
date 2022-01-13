de49e96c82a0fd6a31283d047289b36d1bee3b6f
Anamika <anamikarecsonbhadra@gmail.com>
2022-01-13 15:25:08 +0530

    new navbar is added

---
+++ src/Components/Navbar/Navbar2.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import styled from 'styled-components'
+// import BasicModal from './LoginModal'
+import styles from './navbar.module.css'
+// import LanguageIcon from '@mui/icons-material/Language';
+// import SignupModal from './SignupModal';
+import TextField from '@mui/material/TextField';
+import MessageIcon from '@mui/icons-material/Message';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import AssignmentIndIcon from '@mui/icons-material/AssignmentInd';
+
+
+const Nav = styled.nav`
+display:flex;
+justify-content:space-between;
+padding:14px;
+background-color:white;
+`
+
+const IconImage = styled.img`
+  height: 40px;
+  padding-top: 5px;
+  padding-bottom: 5px;
+`;
+
+
+
+
+
+export default function Navbar2() {
+    return (
+        <Nav className={styled.navComp}>
+           
+            <logoCont>
+                <img  src="./logo.svg" alt="logo-img" style={{marginRight:"30px"}} />
+                
+            <TextField id="outlined-basic" label="Search for keywords" variant="outlined" />
+            <TextField id="outlined-basic" label="City or zip code" variant="outlined" />
+          <button style={{backgroundColor:"tomato",border:"none",borderTopRightRadius:"5px",borderBottomRightRadius:"5px"}}>  <IconImage
+          src="https://cdn-icons-png.flaticon.com/512/6583/6583229.png"
+          alt="icon"
+          ByteLengthQueuingStrategy
+        /></button>
+            </logoCont>
+           
+            <div className={styles.linkDiv}>
+                <div style={{marginleft: 10,fontSize:"20px"}}>
+                    <MessageIcon/>
+                </div>
+            
+                <div style={{marginleft: 10,fontSize:"20px"}}>
+                     <NotificationsIcon/>
+               </div>
+                   
+                <div style={{marginleft: 10,fontSize:"20px"}}>
+                    <AssignmentIndIcon/>
+                </div>
+                    
+              
+            </div>
+        </Nav>
+    )
+}
