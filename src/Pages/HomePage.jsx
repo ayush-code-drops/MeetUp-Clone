740be5d7d631444a4bfad54e08a8c28e4de3dc02
Anamika <anamikarecsonbhadra@gmail.com>
2022-01-14 12:17:27 +0530

    complete create a new group functionality is added

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -71,22 +71,34 @@ const Home = () => {
                     </div>
                
             </div>
-
-            <div>
-                <h3>Your Created Groups will show here.</h3>
-
-                <div style={{backgroundColor:"white",display:"flex",padding:"3rem",margin:"20px",width:"50%"}}>
-                    <div>
-                        <img src="https://secure-content.meetupstatic.com/images/classic-events/500613123/333x188.webp" alt="img" style={{height:"150px",width:"150px",borderRadius:"10px",marginRight:"10px"}}/>
+            {/* if (name)  ( */}
+                {/* { count && <h1>Messages: {count}</h1>} */}
+                {
+                    name && <div>
+                    <h3>Your Created Groups will show here.</h3>
+    
+                    <div style={{backgroundColor:"white",display:"flex",padding:"3rem",margin:"20px",width:"50%",margin:"auto"}}>
+                        <div>
+                            <img src="https://secure-content.meetupstatic.com/images/classic-events/500613123/333x188.webp" alt="img" style={{height:"150px",width:"150px",borderRadius:"10px",marginRight:"10px"}}/>
+                        </div>
+                        <div style={{textAlign:"left"}}>
+                            <h3>Event Name : {name}</h3>
+                            <p><strong>Event Location : </strong>{location}</p>
+                            <p> <strong> Event Description :</strong> Warm welcome from Nepal Cloud Professional's community! In Azure Saturday, we are planning to come up with following topics of discussions:</p>
+                            <p>----------------------------------</p>
+                            <p> <strong>{" "}{" "}{" "}  SESSION DETAILS </strong></p>
+                            <p>----------------------------------</p>
+                            <p>11:00am – 11:30am - Getting started with Azure Cognitive Services</p>
+                            <p></p>
+                        </div>
+                   
                     </div>
-                    <div style={{textAlign:"left"}}>
-                        <h3>Event Name : {name}</h3>
-                        <p>Event Location : {location}</p>
-   
                     </div>
-               
-                </div>
-            </div>
+                }
+                
+            {/* ) */}
+           
+            
             {/* <TealButton/> */}
             <Footer/>
         </div>
